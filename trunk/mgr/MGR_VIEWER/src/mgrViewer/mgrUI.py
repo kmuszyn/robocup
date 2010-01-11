c4f69cd24103ac683e96a33bd5f394e5fa0d86c4
muszynski.kamil@gmail.com <muszynski.kamil@gmail.com@6b840c79-0841-0410-82d8-bd0d8487c5b7>
2010-01-11 21:34:11 +0000

    UI skeleton finished

--- trunk/mgr/MGR_VIEWER/src/mgrViewer/mgrUI.py
+++ trunk/mgr/MGR_VIEWER/src/mgrViewer/mgrUI.py
@@ -16,32 +16,51 @@ class MgrUI:
     
     def create_main_window(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.window.set_border_width(20)
+        self.window.set_border_width(0)
+        self.window.set_default_size(300,200)
+        self.window.set_title('Mgr Viewer 1.0')
         self.window.connect('delete_event', self.delete_event)
         self.window.show()
+        
                    
+        self.winBox = gtk.VBox(False,0)
+        self.winBox.show()
+        self.window.add(self.winBox)        
+        self.create_top_menu()
+        
+    def create_top_menu(self):
+        menu_item = gtk.MenuItem("File")
+        menu_item.show()
+        menu_bar = gtk.MenuBar()
+        menu_bar.show()
+        menu_bar.append(menu_item)
+        self.winBox.pack_start(menu_bar, False, False, 0)
+        
+        
+    def create_drawning_area(self):        
         self.mainBox = gtk.HBox(False,0)
         self.mainBox.show()
-        self.window.add(self.mainBox)
+        self.winBox.pack_start(self.mainBox)                
         
-    def create_drawning_area(self):
         self.drawing_area = gtk.DrawingArea()
         self.drawing_area.set_size_request(300, 200)
+        self.drawing_area.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("white"))
         self.drawing_area.show()
         self.mainBox.pack_start(self.drawing_area, True, True, 0)
         
-    def create_top_menu(self):
-        menu_bar = gtk.MenuBar()
-        gtk.Container.add(menu_bar)
+    
         
     def create_menu(self):
+        self.menuBox = gtk.VBox(False,0)
+        self.menuBox.show()
+        self.mainBox.pack_start(self.menuBox)
+              
         label = gtk.Label('Step: ')
-        label.show()
-        self.mainBox.pack_start(label, True, True, 0)
+        label.show()        
+        self.menuBox.pack_start(label, True, True, 0)
         
     def __init__(self):
         self.create_main_window()
-        self.create_drawning_area()
-        self.create_top_menu()
+        self.create_drawning_area()        
         self.create_menu()
         gtk.main()
