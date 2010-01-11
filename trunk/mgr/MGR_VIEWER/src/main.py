44ef1f5abba9acb9c4271d36449c1a90235c6ec1
muszynski.kamil@gmail.com <muszynski.kamil@gmail.com@6b840c79-0841-0410-82d8-bd0d8487c5b7>
2010-01-11 19:32:09 +0000

    Package structure created

--- trunk/mgr/MGR_VIEWER/src/main.py
+++ trunk/mgr/MGR_VIEWER/src/main.py
@@ -1,5 +1,6 @@
 import pygtk
 import gtk
+from mgrViewer.videoData import VideoData
 
 class MainWindow:
     def main(self):
@@ -18,11 +19,16 @@ class MainWindow:
         gtk.main_quit()
 
     def __init__(self):
+        #main window settings
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_border_width(20)
         self.window.connect("delete_event", self.delete_event)
         self.window.connect("destroy_event", self.destroy_event)
         self.window.show()
+        
+        self.mainBox = gtk.HBox(False,0)
+        self.mainBox.show()
+        self.window.add(self.mainBox)
         
         
         self.button = gtk.Button("Hello world")
@@ -30,10 +36,15 @@ class MainWindow:
         self.button.connect_object("clicked", gtk.Widget.destroy, self.window)
         self.button.show()
         
-        self.window.add(self.button)
+        self.button2 = gtk.Button("Hello world")        
+        self.button2.show()
+        
+        self.mainBox.pack_start(self.button, True, True, 0)
+        self.mainBox.pack_start(self.button2, True, True, 0)
 if __name__=="__main__":
     #main routine start
     print 'Start'
-    w = MainWindow()
-    w.main()
+    #w = MainWindow()
+    #w.main()
+    vd = VideoData('/home/kamil/workspace/robocup_mgr_client/Debug/videoServer.txt')
     
