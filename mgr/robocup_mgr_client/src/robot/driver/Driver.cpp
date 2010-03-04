524f8fda90963066a3dd8e4809412f2d1ff7618b
muszynski.kamil <muszynski.kamil@6b840c79-0841-0410-82d8-bd0d8487c5b7>
2010-03-04 20:45:31 +0000

    Beta version of trapezoid speed profile, seems to work, but some leaks are present, probably from gazebo

--- mgr/robocup_mgr_client/src/robot/driver/Driver.cpp
+++ mgr/robocup_mgr_client/src/robot/driver/Driver.cpp
@@ -54,14 +54,27 @@ void Driver::goToPosition(Position2d * pos){
 	currSpeed.x = posIface->data->velocity.pos.x;
 	currSpeed.y = posIface->data->velocity.pos.y;
 	//std::cout<<"Curr speed: "<<currSpeed<<std::endl;
-	diff = DynWindow::calcSpeed(modelName, pos->pos, currSpeed);
+	//diff = DynWindow::calcSpeed(modelName, pos->pos, currSpeed);
 
+//	diff.x = MotionControl::calcSpeed(currSpeed.x, curr->pos.x,pos->pos.x);
+//	diff.y = MotionControl::calcSpeed(currSpeed.y, curr->pos.y,pos->pos.y);
+//
+//	std::cout<<"**** Test 1\n";
+//	Vector2d result2 = MotionControl::calcSpeed(Vector2d(2,0), Vector2d(0,0), Vector2d(1,0));
+//	std::cout<<"res: "<<result2<<std::endl;
+//
+//	std::cout<<"**** Test 2\n";
+//	result2 = MotionControl::calcSpeed(Vector2d(0,2), Vector2d(0,0), Vector2d(1,0));
+//	std::cout<<"res: "<<result2<<std::endl;
 
 	//double scale = 2.0;
 	//diff = diff * (scale / diff.length());
 
-
+	diff = MotionControl::calcSpeed(currSpeed, curr->pos, pos->pos);
+//	std::cout<<"Current robot rotation: "<<curr->rot.val<<std::endl;
 	diff = diff.rotate(-(curr->rot.val));
+
+//	std::cout<<"result after rotate: "<<diff<<std::endl;
 
 	//calculating rotation speed
 
@@ -73,7 +86,7 @@ void Driver::goToPosition(Position2d * pos){
 
 	double vRot = (angle2rot)*3;
 
-//	std::cout<<"Calculated order: "<<diff<<std::endl;
+	//std::cout<<"Calculated order: "<<diff<<std::endl;
 //	std::cout<<"Rotacja: "<<curr->rot.val<<std::endl;
 //	std::cout<<"Poz: " << curr->pos<<std::endl;
 
@@ -134,4 +147,90 @@ void Driver::doTest(){
 //	std::cout.precision(6);
 //	std::cout<<"driver::test: "<<this->currPos.pos<<" "<<v<<" "<<w<<std::endl;
 
+
+
+}
+
+Vector2d
+Driver::MotionControl::calcSpeed(const Vector2d & currSpeed,
+								 const Vector2d & currPos,
+								 const Vector2d & dest)
+{
+	double VMAX = 2.0;
+	double a = 8.3;
+	double THRESHOLD = 0.01;
+	Vector2d result;
+	//disassembling currSpeed to 2 parts: pararell to vector to dest and perpendicular
+	Vector2d destVec = dest - currPos;
+
+//	std::cout<<"***************\nCurr pos: "<<currPos<<" dest:"<<dest<<std::endl;
+//	std::cout<<"dest vec: "<<destVec<<std::endl;
+
+	double angleToDest = destVec.angleTo(currSpeed);
+	double parallel = currSpeed.length() * cos(angleToDest);
+	double perpendicular = currSpeed.length() * sin(angleToDest);
+
+//	std::cout<<"Parallel: "<<parallel<<" perpendicular: "<<perpendicular<<std::endl;
+
+	//perpendicular part must be close to 0
+	if (fabs(perpendicular)>THRESHOLD )
+		perpendicular = -perpendicular;
+
+	if (parallel < 0 && fabs(parallel) > THRESHOLD) //breaking if this part is in opposite dir
+		parallel = -parallel;
+	else
+	{
+		parallel = sqrt(2 * (dest-currPos).length() * a);
+		if (parallel > VMAX) parallel = VMAX;
+	}
+
+//	std::cout<<"New values:\n";
+//	std::cout<<"Parallel: "<<parallel<<" perpendicular: "<<perpendicular<<std::endl;
+
+
+	result.x = parallel;
+	result.y = perpendicular;
+//	std::cout<<"Result: "<<result<<std::endl;
+
+
+	//creating new vec by projection of result vec to OX
+	double angle2OX = Vector2d(1,0).angleTo(destVec);
+//	std::cout<<"angle2ox "<<angle2OX<<std::endl;
+//	result.x = result.length() * cos(angle2OX);
+//	result.y = result.length() * sin(angle2OX);
+	result = result.rotate(angle2OX);
+
+//	std::cout<<"Result after rotating OX: "<<result<<std::endl;
+
+	return result;
+}
+
+double Driver::MotionControl::calcSpeed(double currSpeed, double currPos, double dest)
+{
+	//based on www.cs.cmu.edu/~mmv/papers/05nrl-jim.pdf, page 7
+
+	double VMAX = 2.0;
+	double a = 8.3;
+	double dist = fabs(dest - currPos);
+	double sgn = currSpeed > 0 ? 1 : -1;
+
+	//desired speed direciton:
+	double dir = dest - currPos;
+
+	//1.
+	if ( (currSpeed * dir) < 0)	//both have different signs, BREAKING
+		return -currSpeed;
+
+	//2.
+	if (fabs(currSpeed) >= sqrt(2 * dist * a)) //BREAK
+		return -currSpeed;
+
+	//3.
+	if (fabs(currSpeed) > VMAX + 0.01) //reduce to vmax
+		return sgn * VMAX;
+
+	//4. simplified for now
+	double result = sqrt(2 * (dist) * a);
+	if (result > VMAX) result = VMAX;
+	return sgn * result;
 }
--- mgr/robocup_mgr_client/src/robot/driver/Driver.h
+++ mgr/robocup_mgr_client/src/robot/driver/Driver.h
@@ -47,6 +47,13 @@ private:
 	std::string modelName; //TODO: to jest nadmiarowe... ale uzywane do zapisu danych do debugowania
 
 	static log4cxx::LoggerPtr logger;
+
+	class MotionControl
+	{
+	public:
+		static double calcSpeed(double currSpeed, double currPos, double dest);
+		static Vector2d calcSpeed(const Vector2d & currSpeed, const Vector2d & currPos, const Vector2d & dest);
+	};
 };
 
 #endif /* DRIVER_H_ */
